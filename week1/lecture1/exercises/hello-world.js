00b6c7d807c597bb583e328ab574ddd27207ac27
Petter Karlsson <petter.karlsson@cmeducations.se>
2022-05-03 15:05:34 +0200

    loop

--- week1/lecture1/exercises/hello-world.js
+++ week1/lecture1/exercises/hello-world.js
@@ -1,4 +1,8 @@
 const msg = "hello";
-const msg2 = " world";
+const msg2 = " world ";
 const msg3 = msg + msg2;
-console.log(msg3);
+let i = 0;
+while (i < 50) {
+  console.log(msg3 + i);
+  i++;
+}
