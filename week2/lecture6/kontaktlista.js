e8adc32a0657ba8b92093dffd3c91a99aa8fc906
Petter Karlsson <petter.karlsson@cmeducations.se>
2022-05-15 15:48:31 +0200

    kontaktlista klar

--- week2/lecture6/kontaktlista.js
+++ week2/lecture6/kontaktlista.js
@@ -39,7 +39,7 @@ function getDateStamp() {
   if (minute < 10) minute = "0" + minute;
   let second = now.getSeconds();
   if (second < 10) second = "0" + second;
-  const dateStamp = `${days[day]} den ${date} ${months[month]} ${
+  const dateStamp = `${days[day]} ${date} ${months[month]} ${
     year + 1900
   } Kl ${hour}:${minute}:${second}`;
   return dateStamp;
@@ -56,25 +56,39 @@ fs.readFile("./people.json", (error, data) => {
 const server = http.createServer((req, res) => {
   console.log(`${req.method} till url: ${req.url}`);
   const items = req.url.split("/");
-  let id = 0;
+  //let id = 0;
+  let oName;
+  let oId;
+  let oNumber;
   if (items[3]) {
-    id = parseInt(items[3]);
+    const id = parseInt(items[3]);
+    people.forEach((element, i) => {
+      if (parseInt(element.id) === id) {
+        oName = element.name;
+        oId = element.id;
+        oNumber = element.number;
+      }
+    });
   }
 
-
-  if (items[1] === "api" && items[2] === "persons") {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/html");
-    res.write(documentStart);
-    res.write(`
+  if (items[1] === "api" && items[2] === "persons" && items[3]) {
+    if (oId === undefined) {
+      res.statusCode = 404;
+    } else {
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/html");
+      res.write(documentStart);
+      res.write(`
             <h1>Person</h1>
             `);
-    res.write(`
-        <p>${people[id].name}</p>
-        <p>${people[id].number}</p>
+      res.write(`
+        <p>${oId}</p>
+        <p>${oName}</p>
+        <p>${oNumber}</p>
         <p>${getDateStamp()}</p>
       `);
       res.write(documentEnd);
+    }
   }
   if (items[1] === "info") {
     res.statusCode = 200;
@@ -96,19 +110,46 @@ const server = http.createServer((req, res) => {
       //console.log(chunk.toString());
       const data = chunk.toString();
       const newPerson = JSON.parse(data);
-      people.push(newPerson);
-
-      const newJson = JSON.stringify(people, null, 2);
-
-      fs.writeFile("./people.json", newJson, (error) => {
-        if (error) {
-          console.log("Filen kunde inte skrivas");
-          return;
+      people.forEach((element, i) => {
+        if (newPerson.id === element.id) {
+          res.statusCode = 409;
+          console.log(`ID ${element.id} finns redan`);
         }
-        console.log(data);
+        if (newPerson.name === element.name) {
+          res.statusCode = 409;
+          console.log(`Namn ${element.name} finns redan`);
+        }
       });
-    });
+      if (res.statusCode !== 409) {
+        people.push(newPerson);
 
+        const newJson = JSON.stringify(people, null, 2);
+
+        fs.writeFile("./people.json", newJson, (error) => {
+          if (error) {
+            console.log("Filen kunde inte skrivas");
+            return;
+          }
+          console.log(data);
+        });
+        res.statusCode = 201;
+      }
+    });
+  } else if (req.method === "DELETE" && !isNaN(parseInt(items[3]))) {
+    people.forEach((element, i) => {
+      if (element.id === parseInt(items[3])) {
+        if (i == 0) people.splice(0, 1);
+        people.splice(i, i);
+        console.log(`Delete ${element.id}`);
+        console.log(`Delete ${element.name}`);
+
+        const stringifiedJson = JSON.stringify(people, null, 2);
+        fs.writeFile("./people.json", stringifiedJson, (err) => {
+          if (err) throw err;
+          console.log("Wrote to people.json");
+        });
+      }
+    });
     res.statusCode = 201;
   } else {
     res.statusCode = 404;
@@ -123,10 +164,15 @@ server.listen(port, () => {
 
 /*
 
-fetch("http://localhost:5000/info", {
-method: "POST",
-body: JSON.stringify({ id: 5, name: "Ryan Dahl", number: "666" }),
-headers: { "Content-Type": "application/json" }
-})
+    fetch("http://localhost:5000/info", {
+    method: "POST",
+    body: JSON.stringify({ id: 5, name: "Ryan Dahl", number: "666" }),
+    headers: { "Content-Type": "application/json" }
+    })
+
+
+    fetch("http://localhost:5000/api/persons/1", {
+    method: "DELETE"
+    })
 
 */
