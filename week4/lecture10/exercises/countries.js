bcce37e2705385312bc429e18952a9a72f2376ea
Petter Karlsson <petter.karlsson@cmeducations.se>
2022-05-24 21:53:32 +0200

    countires

--- week4/lecture10/exercises/countries.js
+++ week4/lecture10/exercises/countries.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const querystring = require("querystring");
 const app = express();
 const port = 4000;
 const fs = require("fs");
@@ -12,13 +11,34 @@ fs.readFile("./countries.json", (error, data) => {
   countries = JSON.parse(data);
 });
 
-app.get("/", (req, res) => {
+app.get("/countries/:countrycode", function (req, res) {
+  let cCode = req.params.countrycode;
+  const country = countries.filter((country) => country.alpha2Code === cCode); //alpha2Code
+  res.send(country);
+});
+
+app.get("/countries", (req, res) => {
+  let viewCountries = [];
   let page = parseInt(req.query.page);
   let size = parseInt(req.query.size);
-  res.send(`Page ${page}, size ${size}`);
-  console.log(`Page ${page + size}`);
+  let end = (page * 2 * size) / 2;
+  let start = end - size;
+  let viewI = 0;
+  for (let i = start; i < end; i++) {
+    if (i >= countries.length) break;
+    else {
+      viewCountries[viewI] = countries[i].name;
+      viewI++;
+    }
+  }
+
+  res.send(viewCountries);
+  console.log(viewCountries);
+  console.log(viewCountries.length);
   res.end();
 });
+app.get("/countries", (req, res) => {});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
