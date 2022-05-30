cfadd71f321473e91cd30d2fe0b0a434a07f21f4
Petter Karlsson <petter.karlsson@cmeducations.se>
2022-05-30 13:52:49 +0200

    cars routers

---
+++ express/car-registry/routers/cars.router.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const carsController = require("../controllers/cars.controller");
+const carsRouter = express.Router();
+
+const morgan = require("morgan");
+carsRouter.use(morgan("dev"));
+
+carsRouter.get("/cars", carsController.getCars);
+carsRouter.get("/cars/:id", carsController.getCar);
+carsRouter.post("/cars", carsController.postCar);
+carsRouter.delete("/cars/:id", carsController.deleteCar);
+carsRouter.put("/cars/:id", carsController.putCar);
+carsRouter.patch("/cars/:id", carsController.patchCar);
+
+module.exports = carsRouter;
--- express/car-registry/server.js
+++ express/car-registry/server.js
@@ -1,28 +1,11 @@
 const express = require("express");
 const app = express();
 const port = 4000;
-const ownersController = require("./controllers/owners.controller");
-const carsController = require("./controllers/cars.controller");
 
 app.use(express.json());
 
-const ownersRouter = express.Router();
-const carsRouter = express.Router();
-
-ownersRouter.get("/owners", ownersController.getOwners);
-ownersRouter.get("/owners/:id", ownersController.getOwner);
-ownersRouter.post("/owners", ownersController.postOwner);
-ownersRouter.delete("/owners/:id", ownersController.deleteOwner);
-ownersRouter.put("/owners/:id", ownersController.putOwner);
-ownersRouter.patch("/owners/:id", ownersController.patchOwner);
-
-carsRouter.get("/cars", carsController.getCars);
-carsRouter.get("/cars/:id", carsController.getCar);
-carsRouter.post("/cars", carsController.postCar);
-carsRouter.delete("/cars/:id", carsController.deleteCar);
-carsRouter.put("/cars/:id", carsController.putCar);
-carsRouter.patch("/cars/:id", carsController.patchCar);
-
+const ownersRouter = require("./routers/owners.router");
+const carsRouter = require("./routers/cars.router");
 
 app.use(ownersRouter);
 app.use(carsRouter);
@@ -30,5 +13,3 @@ app.use(carsRouter);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
-
