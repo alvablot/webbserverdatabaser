f524234e0c8a2503643535abb33bb9a5ddecabc7
Petter Karlsson <petter.karlsson@cmeducations.se>
2022-06-11 16:06:15 +0200

    lördag

--- K2/database.js
+++ K2/database.js
@@ -6,53 +6,41 @@ const db = new sqlite3.Database("./data/books_db.db", (error) => {
     console.error(error.message);
     throw error;
   }
-
-  const usersStmt = `
-  CREATE TABLE users (
+  function initTable(query) {
+    const exist = new Promise((resolve, reject) => {
+      db.get(`SELECT * FROM users`, (err, rows) => {
+        resolve(rows);
+        console.log(`${exist.json()}`);
+      });
+    });
+    return new Promise((resolve, reject) => {
+      db.run(query, (err, rows) => {
+        resolve(rows);
+        console.log(`WOW! I JUST DID ${query.substring(0, 18)}`);
+      });
+    });
+  }
+  const createUsersTable = `CREATE TABLE users (
     id VARCHAR (255) PRIMARY KEY UNIQUE,
     first_name VARCHAR (255),
     last_name VARCHAR (255),
-    email VARCHAR (255),
+    email VARCHAR (255) UNIQUE,
     password CHAR (60) DEFAULT (666) 
   )
   `;
-  db.run(usersStmt, (error) => {
-      const insert =
-        "INSERT INTO users (id, first_name, last_name, email, password) VALUES (?, ?, ?, ?, ?)";
-      db.run(insert, [
-        uuid.v4(),
-        "Ryan",
-        "Dahl",
-        "ryan@dahl.dk",
-        md5("666"),
-      ]);
-  });
+  const createBooksTable = `CREATE TABLE books (
+      id VARCHAR (255) PRIMARY KEY UNIQUE,
+      title VARCHAR (255),
+      author VARCHAR (255),
+      isbn VARCHAR (255),
+      publication_date DATE,
+      binding VARCHAR (255),
+      user_id VARCHAR (255) 
+    )
+    `;
 
-  const booksStmt = `
-  CREATE TABLE books (
-    id VARCHAR (255) PRIMARY KEY UNIQUE,
-    title VARCHAR (255),
-    author VARCHAR (255),
-    isbn VARCHAR (255),
-    publication_date DATE,
-    binding VARCHAR (255),
-    user_id VARCHAR (255) 
-  )
-  `;
-
-  db.run(booksStmt, (error) => {
-      const insert =
-        "INSERT INTO books (id, title, author, isbn, publication_date, binding, user_id) VALUES (?, ?, ?, ?, ?, ?, ?)";
-      db.run(insert, [
-        uuid.v4(),
-        "Boktitel",
-        "Författare",
-        "ISBN-nummer",
-        "1971-01-01",
-        "Pocket",
-        "NULL",
-      ]);
-  });
+  initTable(createUsersTable);
+  initTable(createBooksTable);
 
   console.log("Ansluten till books_db");
 });
